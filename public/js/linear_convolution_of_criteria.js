be902ea71f36c54827cd967158b269ff033a91b7
Alexandr Vasiliev <alexandr.vasiliev1994@gmail.com>
2016-06-02 10:05:18 +0300

    delete comments

--- public/js/linear_convolution_of_criteria.js
+++ public/js/linear_convolution_of_criteria.js
@@ -1,32 +1,31 @@
-// $(function () {
-//     alert('sd');
-//     $('form').on('submit', function (e) {
-//         e.preventDefault();
-//         $.ajax({
-//             type: 'post',
-//             url: '/linearConvolution',
-//             data: $('form').serialize(),
-//             success: function (res) {
-//                 res = JSON.parse(res);
-//                 debugger
-//                 var tdCoef = $('td#' + res.coef.alternative), tdPriority = $('td#' + res.priority.alternative);
-//                 for(var i = 1; i <= res.coef.alternative; i++) {
-//                     debugger
-//                     $('td#' + i).text('E' + i);
-//                 }
-//                 $("td").removeClass('danger');
-//                 //
-//                 tdCoef.text(tdCoef.text() + ' by coef');
-//                 tdPriority.text(tdPriority.text() + ' by priority');
-//                 tdCoef.addClass('danger');
-//                 tdPriority.addClass('danger');
-//                 var coefTable = buildCoefTable(res.coef);
-//                 //$('div#response').text(coefTable);
-//             }
-//         });
-//     });
-// });
-//
+$(function () {
+    $('form').on('submit', function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: 'post',
+            url: '/linearConvolution',
+            data: $('form').serialize(),
+            success: function (res) {
+                res = JSON.parse(res);
+                debugger
+                var tdCoef = $('td#' + res.coef.alternative), tdPriority = $('td#' + res.priority.alternative);
+                for(var i = 1; i <= res.coef.alternative; i++) {
+                    debugger
+                    $('td#' + i).text('E' + i);
+                }
+                $("td").removeClass('danger');
+                //
+                tdCoef.text(tdCoef.text() + ' by coef');
+                tdPriority.text(tdPriority.text() + ' by priority');
+                tdCoef.addClass('danger');
+                tdPriority.addClass('danger');
+                var coefTable = buildCoefTable(res.coef);
+                //$('div#response').text(coefTable);
+            }
+        });
+    });
+});
+
 function buildCoefTable(data)
 {
 
